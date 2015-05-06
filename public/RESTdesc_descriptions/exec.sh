af83c7d3306d2d940383ef60e7955ad251c1b1aa
Ventura Daniela <daniela1988@gmail.com>
2015-05-06 17:12:04 +0200

    Added RESTdesc description for get hourly forecast

--- public/RESTdesc_descriptions/exec.sh
+++ public/RESTdesc_descriptions/exec.sh
@@ -1,2 +1,2 @@
-eye --traditional --quick-answere --tactic transaction --query goal02.n3 preference.n3 descriptions/* inference/* > outputs/out.n3
+eye --traditional --quick-answere --tactic transaction --query goal03.n3 preference.n3 descriptions/* inference/* > outputs/out.n3
 eye --traditional outputs/out.n3 parser/services.n3 --query parser/composition.n3 --nope
